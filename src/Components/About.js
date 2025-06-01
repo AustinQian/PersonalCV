dfb0c648d93ec77ce7bbee39d2ee278eebc25962
AustinQian <ubunteen2@gmail.com>
2025-06-01 16:55:47 -0400

    Debug #3

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,6 @@ class About extends Component {
     if (!this.props.data) return null;
 
     const name = this.props.data.name;
-    const profilepic = "images/" + this.props.data.image;
     const bio = this.props.data.bio;
     const phone = this.props.data.phone;
     const email = this.props.data.email;
